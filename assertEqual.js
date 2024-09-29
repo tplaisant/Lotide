2d06595299d76720722e0cc0ee54055a2e68fcb9
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-09-28 19:38:22 -0600

    Second commit

--- assertEqual.js
+++ assertEqual.js
@@ -3,12 +3,14 @@ const assertEqual = function(actual, expected) {
   let thumbsDown = String.fromCodePoint(0x1F44E);
 
   if (actual === expected) {
-    console.log(thumbsUp + thumbsUp + thumbsUp + 'Assertion Passed: ' + actual + ' === ' + expected);
+    console.log(`${thumbsUp}${thumbsUp}${thumbsUp} Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log(thumbsDown + thumbsDown + thumbsDown + 'Assertion Failed: ' + actual + ' !== ' + expected);
+    console.log(`${thumbsDown}${thumbsDown}${thumbsDown} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
 // TEST CODE
 assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);//123
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(1, 2);
