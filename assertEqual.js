7cfe41f8efba1de03460c9070bc244e4fe66fbfc
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-09-28 19:29:47 -0600

    First try

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+const assertEqual = function(actual, expected) {
+  let thumbsUp = String.fromCodePoint(0x1F44D);
+  let thumbsDown = String.fromCodePoint(0x1F44E);
+
+  if (actual === expected) {
+    console.log(thumbsUp + thumbsUp + thumbsUp + 'Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log(thumbsDown + thumbsDown + thumbsDown + 'Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);//123
