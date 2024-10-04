e38ebb33e2f0cb35d5a83ca1cb3d32f07efd9a9e
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-10-03 20:59:24 -0600

    Second commit

--- countLetters.js
+++ countLetters.js
@@ -13,15 +13,42 @@ const countLetters = function(sentence) {
   let result = {};
 
   for (let letter of sentence) {
-    if (result[letter] === undefined) {      
-      result[letter] = 1;
-    } else {
-      result[letter] = result[letter] + 1;
-    }    
+    let capitalLetter = letter.toUpperCase();
+    if (capitalLetter !== ' '){
+      if (result[capitalLetter] === undefined) {      
+        result[capitalLetter] = 1;
+      } else {
+        result[capitalLetter] = result[capitalLetter] + 1;
+      }    
+    }
   }
   return result;
 }
 
 const result1 = countLetters('LHL');
-
 assertEqual(result1["L"], 2);
+assertEqual(result1["H"], 1);
+
+const result2 = countLetters('Lighthouse Labs');
+assertEqual(result2["L"], 2);
+assertEqual(result2["H"], 2);
+assertEqual(result2["S"], 2);
+assertEqual(result2["I"], 1);
+assertEqual(result2["G"], 1);
+assertEqual(result2["T"], 1);
+assertEqual(result2["O"], 1);
+assertEqual(result2["U"], 1);
+assertEqual(result2["E"], 1);
+assertEqual(result2["A"], 1);
+assertEqual(result2["B"], 1);
+
+
+const result3 = countLetters('Another test');
+assertEqual(result3["T"], 3);
+assertEqual(result3["E"], 2);
+assertEqual(result3["A"], 1);
+assertEqual(result3["N"], 1);
+assertEqual(result3["O"], 1);
+assertEqual(result3["H"], 1);
+assertEqual(result3["R"], 1);
+assertEqual(result3["S"], 1);
