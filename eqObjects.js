6a7036fb8f6697bbe2847669d87efa229b9da5fe
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-10-06 20:15:54 -0600

    Second commit

--- eqObjects.js
+++ eqObjects.js
@@ -4,7 +4,7 @@ const assertEqual = function(actual, expected) {
 
   if (actual === expected) {
     console.log(`${thumbsUp}${thumbsUp}${thumbsUp} Assertion Passed: ${actual} === ${expected}`);
-  } else {
+  } else {    
     console.log(`${thumbsDown}${thumbsDown}${thumbsDown} Assertion Failed: ${actual} !== ${expected}`);
   }
 };
@@ -24,18 +24,11 @@ const eqArrays = function(actual, expected) {
 // Returns true if both objects have identical keys with identical values.
 // Otherwise you get back a big fat false!
 const eqObjects = function (object1, object2) {
-
   if (Object.keys(object1).length !== Object.keys(object2).length) return false; // Check number of properties for both objects
-
   for (let key in object1) {    
     if (Array.isArray(object1[key]) && Array.isArray(object2[key])) { // Check if both values from both objects are arrays
-      if (Object.keys(object1[key]).length !== Object.keys(object2[key]).length) { // Check number of values for both arrays
-        return false;
-      } else {
-        if (!eqArrays(object1[key], object2[key])) return false;// Check if values inside the arrays are not equal
-      }   
-    }
-    if (object1.key !== object2.key) return false; // Check if values are not equal
+        if (!eqArrays(object1[key], object2[key])) return false;// Check if values and qty of elements inside the arrays are equal
+    } else if (object1[key] !== object2[key]) return false; // Check if values are equal (when not an array)
   }
   return true; // If nothing is wrong, return 'true'
 };
