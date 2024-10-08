7513cb4221bec85cff094574c7eee0880884085f
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-10-07 20:11:10 -0600

    Second commit

--- takeUntil.js
+++ takeUntil.js
@@ -1,7 +1,32 @@
+const eqArrays = function(actual, expected) {
+
+  if (actual.length === expected.length) {
+    for (let j = 0; j < actual.length; j++) {
+      if (actual[j] !== expected[j]) {
+        return false;
+      }
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const assertArraysEqual = function(actual, expected) {
+  let thumbsUp = String.fromCodePoint(0x1F44D);
+  let thumbsDown = String.fromCodePoint(0x1F44E);
+
+  if (eqArrays(actual, expected)){
+    console.log(`${thumbsUp}${thumbsUp}${thumbsUp} Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`${thumbsDown}${thumbsDown}${thumbsDown} Assertion Failed: ${actual} !== ${expected}`);
+  }
+}
+
 const takeUntil = function(array, callback) {
   let results = [];
 
-  for (item of array) {
+  for (let item of array) {
     if (callback(item)) {
       return results;
     } else {
@@ -13,10 +38,10 @@ const takeUntil = function(array, callback) {
 
 const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
 const results1 = takeUntil(data1, x => x < 0);
-console.log(results1); // Expected: [ 1, 2, 5, 7, 2 ]
-
-console.log('---');
+// TEST CODE
+assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
 
 const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
 const results2 = takeUntil(data2, x => x === ',');
-console.log(results2); // Expected: [ 'I\'ve', 'been', 'to', 'Hollywood' ]
+// TEST CODE
+assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
