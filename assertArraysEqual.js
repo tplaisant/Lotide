246fc9a595da52926e814ba4b67797a751e4071c
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-10-21 19:19:36 -0600

    Refactoring assertArraysEqual and assertArraysEqualTest

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,4 @@
-const eqArrays = function(actual, expected) {
-
-  if (actual.length === expected.length) {
-    for (let j = 0; j < actual.length; j++) {
-      if (actual[j] !== expected[j]) {
-        return false;
-      }
-    }
-    return true;
-  } else {
-    return false;
-  }
-}
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(actual, expected) {
   let thumbsUp = String.fromCodePoint(0x1F44D);
@@ -23,5 +11,4 @@ const assertArraysEqual = function(actual, expected) {
   }
 }
 
-// TEST CODE
-assertArraysEqual([1, 2, 3], [1, 2, 2]);
+module.exports = assertArraysEqual;
