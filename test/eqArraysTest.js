26ca8621b1ca107559386718860e7430b8181b56
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-10-21 19:12:20 -0600

    refactored eqArraysTest

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -2,5 +2,5 @@ const assertEqual = require("../assertEqual");
 const eqArrays = require("../eqArrays");
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 1, 3], [1, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays([1, 1, 3], [1, 2, 3]), false); // => should FAIL
 assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), true); // => should FAIL
