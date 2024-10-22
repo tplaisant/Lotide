2dc657f2ccf446d8a68347c04c435cdd5d98c3fb
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-10-21 20:18:20 -0600

    middleTest

--- test/middleTest.js
+++ test/middleTest.js
@@ -10,5 +10,11 @@ describe("#middle", () => {
   });  
   it("returns '[]' for '[1, 2]'", () => {
     assert.deepEqual(middle([1, 2]), []);
-  });    
+  });   
+  it("make sure the original array was not altered by the middle function", () => {
+    const words = ["hello", "world", "lighthouse"];
+    const result = middle(words);
+    assert.deepEqual(result, ["world"]);
+    assert.strictEqual(words.length, 3);
+  }); 
 })  
