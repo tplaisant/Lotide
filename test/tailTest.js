8656933bc08e2c5fb4d775bdf8d8bffc5e757634
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-10-21 19:06:40 -0600

    new tailTest

--- test/tailTest.js
+++ test/tailTest.js
@@ -5,10 +5,8 @@ let result = tail(['Test', 'Lighthouse', 'Labs']);
 assertEqual(result.length, 2);
 assertEqual(result[0], 'Lighthouse');
 assertEqual(result[1], 'Labs');
-console.log('HERE');
 let other = tail([5, 6]);
 assertEqual(other[0], 6);
-console.log('HERE');
 
 result = tail(['Test']);
 assertEqual(result.length, 0);
@@ -18,4 +16,3 @@ assertEqual(result.length, 0);
 
 let newArray = ['TEST', 'LIGHTHOUSE', 'LABS'];
 tail(newArray);
-console.log(newArray);
