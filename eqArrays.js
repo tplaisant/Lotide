4532011e7d726f022084d86c47c9e90d200aee46
Tiago Plaisant <tiagoplaisant@hotmail.com>
2024-10-21 19:10:24 -0600

    refactor eqArrays and eqArraysTest

--- eqArrays.js
+++ eqArrays.js
@@ -1,14 +1,3 @@
-const assertEqual = function(actual, expected) {
-  let thumbsUp = String.fromCodePoint(0x1F44D);
-  let thumbsDown = String.fromCodePoint(0x1F44E);
-
-  if (actual === expected) {
-    console.log(`${thumbsUp}${thumbsUp}${thumbsUp} Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`${thumbsDown}${thumbsDown}${thumbsDown} Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const eqArrays = function(actual, expected) {
   if (actual.length === expected.length) {
     for (let j = 0; j < actual.length; j++) {
@@ -22,7 +11,4 @@ const eqArrays = function(actual, expected) {
   }
 }
 
-// TEST CODE
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([1, 1, 3], [1, 2, 3]), true); // => should FAIL
-assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), true); // => should FAIL
+module.exports = eqArrays;
---
+++ test/eqArraysTest.js
@@ -0,0 +1,6 @@
+const assertEqual = require("../assertEqual");
+const eqArrays = require("../eqArrays");
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 1, 3], [1, 2, 3]), true); // => should FAIL
+assertEqual(eqArrays([3, 2, 1], [1, 2, 3]), true); // => should FAIL
